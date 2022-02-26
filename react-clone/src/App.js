9452c0a1e6e20d14e7b84c12858757f234bade02
friendcon <imyhy01@gmail.com>
2022-02-26 22:58:53 +0900

    React useEffect

--- react-clone/src/App.js
+++ react-clone/src/App.js
@@ -3,12 +3,46 @@
  */
 import Button from "./Button";
 import styles from "./App.module.css"
+import {useState, useEffect} from "react";
 
 function App() {
+    // useState는 value, modifier function 반환
+    const[counter, setValue] = useState(0);
+    const[keyword, setKeyword] = useState("");
+    const onClick = () => {
+        setValue((prev) => prev + 1);
+    }
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+    }
+
+    console.log('i run all the time');
+
+    useEffect(()=>{
+        console.log("i run only one");
+    }, []);
+
+    // keyword가 변화할 때 코드를 실행하고 싶다
+    useEffect(()=>{
+        console.log("i run when keyword changes.");
+    }, [keyword]);
+
+    useEffect(()=>{
+        console.log("i run when counter changes.");
+    }, [counter]);
+    useEffect(()=> {
+        console.log("i run when keyword & counter change");
+    }, [counter, keyword]);
+    /*
+        Q. state가 변할 때 component는 다시 render된다.
+        특정 부분이 한번만 실행되게끔 하려면 어떻게 해결해야할까?
+        A. useEffect(한번만 실행하고 싶은 코드, dependency)
+     */
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!!</h1>
-        <Button text={"Continue"}/>
+        <input value={keyword} type="text" placeholder="Search Here..." onChange={onChange}/>
+        <h1>{counter}</h1>
+        <button onClick={onClick}>click me</button>
     </div>
   );
 }
